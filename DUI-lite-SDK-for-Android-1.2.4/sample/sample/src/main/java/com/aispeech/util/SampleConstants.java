a74d4a85fad7290ee64fce853932a5ae50544f23
wei.wu@aispeech.com <wuwei123>
2019-08-08 09:31:29 +0800

    update to v1.2.4

---
+++ DUI-lite-SDK-for-Android-1.2.4/sample/sample/src/main/java/com/aispeech/util/SampleConstants.java
@@ -0,0 +1,51 @@
+package com.aispeech.util;
+
+/**
+ * Created by wuwei on 18-6-6.
+ */
+
+public class SampleConstants {
+
+    //aec res
+    public static final String AEC_RES = "AEC_ch4-2-ch2_2ref_com_20180315_v0.9.0.bin";
+    //wakeup res
+    public static final String WAKEUP_RES = "wakeup_aifar_comm_20180104.bin";
+    //echo module res
+    public static final String ECHO__RES = "AEC_ch2-2-ch1_1ref_common_20180510_v0.9.4.bin";
+    //fdm module res
+    public static final String FDM_RES = "ULA_dualcar_ch2-2-ch2_85mm_common_20180319_v2.0.1.bin";
+    /**
+     * fespcar 模块
+     */
+    public static final String FESPCAR_RES = "UDA_asr_ch2_2_ch2_30mm_20190108_v1.1.0.8.bin";
+    //beamforming res
+    public static final String DUAL_BEAMFORMING_RES = "UDA_asr_ch2_2_ch2_30mm_20190619_v1.1.1.1_nlms0ff_vpOff_kOrgOff.bin";
+    public static final String LINE4_BEAMFORMING_RES = "ULA_asr_ch4_2_ch4_35mm_20190619_v1.1.1.1_asrpost0_vpOff_kOrgOn.bin";
+    public static final String CIRCLE4_BEAMFORMING_RES = "UCA_asr_ch4-2-ch4_72mm_comm_20181220_v1.3.8_asrPost0.bin";
+    public static final String CIRCLE6_BEAMFORMING_RES = "UCA_asr_ch6-2-ch6_72mm_comm_20190220_v1.3.8_wkpMTMode0.bin";
+    //vad module res
+    public static final String VAD_RES = "vad_aihome_v0.10.bin";
+    //local asr module res
+    public static final String EBNFC_RES = "ebnfc.g2p.model4.bin";
+    public static final String EBNFR_RES = "ebnfr.aicar.1.3.0.bin";
+    //local tts module res
+    public static final String TTS_DICT_RES = "aitts_sent_dict_idx_2.0.4_20190527.db";
+    public static final String TTS_DICT_MD5 = "aitts_sent_dict_idx_2.0.4_20190527.db.md5sum";
+    public static final String TTS_FRONT_RES = "local_front.bin";
+    public static final String TTS_FRONT_RES_MD5 = "local_front.bin.md5sum";
+    public static final String TTS_BACK_RES_LUCY = "lucyf_common_back_ce_local.v2.1.0.bin";
+    public static final String TTS_BACK_RES_LUCY_MD5 = "lucyf_common_back_ce_local.v2.1.0.bin.md5sum";
+    public static final String TTS_BACK_RES_ZHILING = "zhilingf_common_back_ce_local.v2.1.0.bin";
+    public static final String TTS_BACK_RES_ZHILING_MD5 = "zhilingf_common_back_ce_local.v2.1.0.bin.md5sum";
+    public static final String TTS_BACK_RES_XIJUN = "xijunm_common_back_ce_local.v2.1.0.bin";
+    public static final String TTS_BACK_RES_XIJUN_MD5 = "xijunm_common_back_ce_local.v2.1.0.bin.md5sum";
+    //vprint module res
+    public static final String ASRPP_RES = "gender_local_model_2019-04-18.bin";
+    public static final String VP_RES = "vprint_aihome_nihaoxiaochi_20190315.bin";
+    public static final String WAKEUP_VP_RES = "vprint_wakeup_aicar_comm_v0.19.0.bin";
+    //semantic module res
+    public static final String ASR_RES = "ngram.foryouge.v0.7.bin";
+    public static final String ASR_NET = "local_asr.net.bin";
+
+    public static final String NR_RES = "NR_ch1-2-ch1_com_20171117_v1.0.0.bin";
+}
